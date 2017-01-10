8b86bf87142d24f8e7b73040931419fc3a1aa1f6
Teemu Roivas <teemu.roivas@metropolia.fi>
2017-01-10 23:31:21 +0200

    app.js some unnecessary code removed + readme

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ var app = express()
 
 app.use(express.static(__dirname + "/public"));
 
-app.get('/', function (req, res) {
-  res.send('lol')
-})
-
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
